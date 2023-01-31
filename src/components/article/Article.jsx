0c83718d3c6de705efe0b63d610b01a1893c713b
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-31 10:19:36 -0300

    Create Article Component

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./article.css";
+const Article = () => {
+  return (
+    <div className="article">
+      <h1>Article Title</h1>
+      <p>Article content</p>
+    </div>
+  );
+};
+export default Article;
