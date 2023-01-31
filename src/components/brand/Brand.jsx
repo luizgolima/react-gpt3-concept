56991b0e6e0cad7993f217b9341aa3b79f7c9d50
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-31 10:22:33 -0300

    Create Brand Component

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,11 @@
+import "./brand.css";
+const Brand = () => {
+  return (
+    <div className="brand">
+      <img src="" alt="" />
+      <h1>React App</h1>
+    </div>
+  );
+};
+
+export default Brand;
