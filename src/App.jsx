5b79e53f8350dded25bd0f78e9d0a2e3f4ce951b
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-31 10:47:47 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
+
 const App = () => {
   return <div></div>;
 };
